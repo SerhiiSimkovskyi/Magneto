12e19db6f9bf9ac4b53c2dd30b0b7325a5756532
Serhii Simkovskyi <serhii.simkovskyi@gmail.com>
2018-05-26 14:27:51 +0300

    Initial Commit

---
+++ Magneto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Magneto
+//
+//  Created by Serhii Simkovskyi on May/26/18.
+//  Copyright © 2018 Serhii Simkovskyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
